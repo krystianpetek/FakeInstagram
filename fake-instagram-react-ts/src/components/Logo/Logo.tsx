590eade1f6de5e2c9c54b2ce557fa4b4f95e5fe8
Krystian Petek <krystianpetek2@gmail.com>
2022-11-11 11:31:35 +0100

    Added content to Header like Logo and Menu Added content and styles to Logo component

--- fake-instagram-react-ts/src/components/Logo/Logo.tsx
+++ fake-instagram-react-ts/src/components/Logo/Logo.tsx
@@ -1,14 +1,12 @@
 import { FunctionComponent } from "react";
 import React, { Route, Routes } from "react-router-dom"
 import "./Logo.scss";
-interface LogoProps {
-
-}
+interface LogoProps { }
 
 const Logo: FunctionComponent<LogoProps> = () => {
     return (
-        <div className="Logo">
-            <h1>LOGO</h1>
+        <div className="Logo" draggable="true">
+            <span className="Logo__Text">FakeInstagram</span>
         </div >
     );
 }
--- fake-instagram-react-ts/src/views/Header/Header.tsx
+++ fake-instagram-react-ts/src/views/Header/Header.tsx
@@ -2,9 +2,7 @@ import "./Header.scss"
 import React, { FunctionComponent } from "react";
 import Logo from "../../components/Logo/Logo";
 import Menu from "../../components/Menu/Menu";
-interface HeaderProps {
-
-}
+interface HeaderProps { }
 
 const Header: FunctionComponent<HeaderProps> = () => {
     return (
