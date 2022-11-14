f164a42cbf2879e21cb39c62c0c5be8d4575618b
Krystian Petek <krystianpetek2@gmail.com>
2022-11-14 21:16:18 +0100

    Changed Title in my profile route, now here is user email who actually are logged

--- fake-instagram-react-ts/src/views/Title/Title.tsx
+++ fake-instagram-react-ts/src/views/Title/Title.tsx
@@ -1,18 +1,21 @@
 import "./Title.scss"
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import TitleHeader from "../../components/TitleHeader/TitleHeader";
+import { ILoginContext, LoginContext } from "../../contexts/LoginContext/LoginContext";
 
 interface TitleProps { }
 
 const Title: FunctionComponent<TitleProps> = () => {
+    const { email } = useContext<ILoginContext>(LoginContext);
+    const myProfile = (email) ? email : "My Profile";
     return (
         <div className="Title">
             <Routes>
                 <Route path="/" element={<TitleHeader title="Home" />}></Route>
                 <Route path="/posts" element={<TitleHeader title="Posts" />}></Route>
                 <Route path="/search" element={<TitleHeader title="Search" />}></Route>
-                <Route path="/myProfile" element={<TitleHeader title="My profile" />}></Route>
+                <Route path="/myProfile" element={<TitleHeader title={`${myProfile}`} />}></Route>
                 <Route path="/login" element={<TitleHeader title="Login" />}></Route>
             </Routes>
         </div>);
