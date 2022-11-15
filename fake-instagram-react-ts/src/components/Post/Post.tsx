808c1e51b34af750be0bafcf82dc2754e095baf3
Krystian Petek <krystianpetek2@gmail.com>
2022-11-15 14:04:48 +0100

    Fix randomColor function to color Posts border

--- fake-instagram-react-ts/src/components/Post/Post.tsx
+++ fake-instagram-react-ts/src/components/Post/Post.tsx
@@ -9,14 +9,21 @@ import "./Post.scss";
 interface PostProps {
     post: IPostResponse
 }
-const randomColor = () => Math.floor(Math.random() * 16777215).toString(16);
+const randomColor = () => {
+    const randomHex = () => Math.floor(Math.random() * 255).toString(16);
+    const r = randomHex();
+    const g = randomHex();
+    const b = randomHex();
+    return `#${r}${g}${b}`;
+}
+
 const Post: FunctionComponent<PostProps> = ({ post }) => {
     const { username } = useContext<ILoginContext>(LoginContext);
     const users = useContext<IUserContext>(UserContext).users;
     const myProfile: IUserResponse = users.find(user => user.id === post.userId)!;
 
     return (
-        <div className="Post" style={{ border: `2px solid #${randomColor()}` }}>
+        <div className="Post" style={{ border: `2px solid ${randomColor()}` }}>
 
             <p className="Post__Username">{myProfile?.name}{myProfile?.username === username && <CgClose style={{ color: "red" }} />}</p>
 
