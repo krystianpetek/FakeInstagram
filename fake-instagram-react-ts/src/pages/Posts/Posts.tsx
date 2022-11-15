ed14db250bc679fc1bb5d8db6e1e61952aff4cc7
Krystian Petek <krystianpetek2@gmail.com>
2022-11-15 20:03:50 +0100

    Change store Posts state from Posts component to PostContext

--- fake-instagram-react-ts/src/pages/Posts/Posts.tsx
+++ fake-instagram-react-ts/src/pages/Posts/Posts.tsx
@@ -1,37 +1,21 @@
 import "./Posts.scss";
-import { useState, FunctionComponent, useEffect } from "react";
+import { useState, FunctionComponent, useContext } from "react";
 import Post from "../../components/Post/Post";
 import IPostResponse from "../../API/IPostResponse";
-import PostService from "../../API/services/PostService";
+import { IPostContext, PostContext } from "../../contexts/PostContext/PostContext";
 
 interface PostsProps { }
 const PostList: FunctionComponent<PostsProps> = () => {
 
-    const getPosts = () => PostService
-        .GetPosts()
-        .then<IPostResponse[]>(response => {
-            if (response.status === 200) {
-                return response.data;
-            }
-            throw new Error("While fetching posts something went wrong!");
-        })
-        .then(response => setPosts(response))
-        .catch(error => {
-            console.log(error);
-            window.location.href = '/errorPage';
-        });
-
-    const [Posts, setPosts] = useState<Partial<IPostResponse[]>>();
-
-    useEffect(
-        () => {
-            getPosts();
-        }, [])
+    const postContext = useContext<IPostContext>(PostContext);
 
     return (
         <div className="Posts">
-            {Posts?.map(x => <Post key={x?.id} post={x!}></Post>).sort(() => Math.random() - 0.5)}
+            {postContext.posts.map(x => <Post key={x?.id} post={x!}></Post>).sort(() => Math.random() - 0.5)}
+            {/* {postContext.posts.map(x => <Post key={x?.id} post={x!}></Post>)} */}
+            { }
         </div >
+
     );
 }
 
