ef80aa31b68468d729460ef128c941ef56e52671
Krystian Petek <krystianpetek2@gmail.com>
2022-11-16 21:59:33 +0100

    Added component InputPostComment with event on enter key down, after click add comments to post Auth that only logged user can add comment (delete WIP)

---
+++ fake-instagram-react-ts/src/components/AddComment/InputPostComment.tsx
@@ -0,0 +1,28 @@
+import { FunctionComponent } from "react";
+import "./InputPostComment.scss";
+type InputType = "text" | "password";
+
+interface InputPostCommentProps {
+    name: string;
+    type: InputType;
+    value?: string | number;
+    placeholder: string;
+    handleKeyDown: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+}
+
+const InputPostComment: FunctionComponent<InputPostCommentProps> = (props) => {
+    const { name, type, placeholder, handleKeyDown } = props;
+    return (
+        <div className="InputPostComment">
+            <input
+                className="InputPostComment__Properties"
+                id={name}
+                name={name}
+                onKeyDown={handleKeyDown}
+                type={type}
+                placeholder={placeholder} />
+        </div>
+    )
+}
+
+export default InputPostComment;
