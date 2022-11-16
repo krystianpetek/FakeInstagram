14f50123c1620df241516f620ea5e3b5d2ded950
Krystian Petek <krystianpetek2@gmail.com>
2022-11-16 22:03:33 +0100

    Modified Post front and backend side, in backend refactoring code added handler for add comment to post removed add comment after click button and now add comment on event keydown enter

--- fake-instagram-react-ts/src/components/Post/Post.tsx
+++ fake-instagram-react-ts/src/components/Post/Post.tsx
@@ -1,4 +1,5 @@
-import { FunctionComponent, useContext, useEffect, useState } from "react";
+import { keyboard } from "@testing-library/user-event/dist/keyboard";
+import React, { FunctionComponent, useContext, useEffect, useState } from "react";
 import { CgClose } from "react-icons/cg";
 import ICommentResponse from "../../API/ICommentResponse";
 import IPostResponse from "../../API/IPostResponse";
@@ -6,6 +7,8 @@ import IUserResponse from "../../API/IUserResponse";
 import { LoginContext, ILoginContext } from "../../contexts/LoginContext/LoginContext";
 import { IPostContext, PostContext } from "../../contexts/PostContext/PostContext";
 import { UserContext, IUserContext } from "../../contexts/UserContext/UserContext";
+import InputPostComment from "../AddComment/InputPostComment";
+import PostComment from "../PostComments/PostComment";
 import "./Post.scss";
 
 const randomColor = () => {
@@ -19,35 +22,49 @@ interface PostProps {
     post: IPostResponse
     comments: Array<ICommentResponse>
 }
-const Post: FunctionComponent<PostProps> = ({ post, /*comments*/ }) => {
-    const { /*username,*/ email } = useContext<ILoginContext>(LoginContext);
-    const users = useContext<IUserContext>(UserContext).users;
-    let myProfile: IUserResponse = users.find(user => user.email === email)!;
-    const posts = useContext<IPostContext>(PostContext);
+const Post: FunctionComponent<PostProps> = (props) => {
+    const { username, email } = useContext<ILoginContext>(LoginContext);
+    const { users } = useContext<IUserContext>(UserContext);
+    const { addComment, removePost } = useContext<IPostContext>(PostContext);
+    const myProfile: (IUserResponse | null) = users.find(user => user.email === email && user.username === username) ?? null;
 
-    // const commtoPost = comments.map(comment => <p key={comment.id}>{comment.id}. {comment.body} {comment.email === myProfile.email && <CgClose style={{ fontSize: "1.2em", color: "red" }} />}</p>);
+    const handleNewComment = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter" && event.currentTarget.value.length !== 0) {
+            addComment({
+                postId: post.id,
+                body: event.currentTarget.value,
+                name: username,
+                email: email
+            });
+            event.currentTarget.value = ""
+        }
+    }
+
+    const { post, comments } = props;
+    const postTitle = `${post?.title[0]?.toUpperCase()}${post?.title?.substring(1)}`;
+    const postUser = users.find(user => user.id === post.userId)!;
+    const postComments = comments.map(comment => {
+        return <PostComment key={comment.id} comment={comment}></PostComment>
+    })
+
     return (
         <div className="Post" style={{ border: `2px solid ${randomColor()}` }}>
-
-            {/* <p className="Post__Username">{myProfile?.name}{myProfile?.username === username && <div style={{ cursor: "pointer" }} onClick={() => { posts.removePost(post.id) }}><CgClose style={{ fontSize: "1.2em", color: "red" }} /></div>}</p> */}
-
-            <p className="Post__Title">{post.title[0]?.toUpperCase()}{post.title.substring(1)}</p>
+            <div className="Post__Header">
+                <p className="Post__Username">{postUser?.name}</p>
+                <div
+                    className="Post__Delete"
+                    onClick={() => { removePost(post.id) }}
+                >{myProfile?.email && <CgClose style={{ fontSize: "1.2em", color: "red" }} />}</div>
+            </div>
+            <p className="Post__Title">{postTitle}</p>
             <p className="Post__Body">{post.body}</p>
-            {/* <div className="Post__Comments">{commtoPost}</div> */}
+            <div className="Post__Comments">{postComments}</div>
 
-
-            <button
-                onClick={() =>
-                    posts.addComment({
-                        postId: post.id,
-                        body: "comment",
-                        email: myProfile?.email,
-                        name: myProfile?.name
-                    }
-                    )
-                }
-
-            >NEW COMMENT</button>
+            {myProfile?.id && <InputPostComment name="newComment"
+                placeholder="Add new comment here!"
+                type="text"
+                key={post.id}
+                handleKeyDown={handleNewComment} />}
         </div >
     );
 }
