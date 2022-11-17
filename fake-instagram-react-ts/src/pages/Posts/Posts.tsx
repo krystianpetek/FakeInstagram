8b5e74b706223a3dd991fe971dfe8714195a37e0
Krystian Petek <krystianpetek2@gmail.com>
2022-11-17 19:48:43 +0100

    Added implementation of adding new post and fixed filtering comments

--- fake-instagram-react-ts/src/pages/Posts/Posts.tsx
+++ fake-instagram-react-ts/src/pages/Posts/Posts.tsx
@@ -3,35 +3,52 @@ import { FunctionComponent, useContext } from "react";
 import Post from "../../components/Post/Post";
 import { IPostContext, PostContext } from "../../contexts/PostContext/PostContext";
 import { ILoginContext, LoginContext } from "../../contexts/LoginContext/LoginContext";
+import { randomColor } from "../../Helpers/randomColor";
+import InputPost from "../../components/InputPost/InputPost";
+import { IUserContext, UserContext } from "../../contexts/UserContext/UserContext";
+import IUserResponse from "../../API/IUserResponse";
 
 interface PostsProps { }
-const PostList: FunctionComponent<PostsProps> = () => {
-    const { email } = useContext<ILoginContext>(LoginContext);
-    const postContext = useContext<IPostContext>(PostContext);
+const Posts: FunctionComponent<PostsProps> = () => {
+    const { email, username } = useContext<ILoginContext>(LoginContext);
+    const { users } = useContext<IUserContext>(UserContext);
+    const myProfile: (IUserResponse | null) = users.find(user => user.email === email && user.username === username) ?? null;
+    const { addPost, posts, comments } = useContext<IPostContext>(PostContext);
 
+    const handleNewPost = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter" && event.currentTarget.value.length !== 0) {
+            addPost({
+                userId: myProfile!.id,
+                title: "",
+                body: event.currentTarget.value
+            });
+            event.currentTarget.value = ""
+        }
+    }
     return (
+
         <div className="Posts">
+            {email &&
+                <div className="Posts__AddNew" style={{ border: `2px solid ${randomColor()}` }}>
+                    <p className="Posts__AddNew__Title">Add new post</p>
+                    <InputPost name="newComment"
+                        placeholder="Add new post here!"
+                        type="text"
+                        handleKeyDown={handleNewPost} />
+                </div>}
 
-
-            {email && <div className="Posts__AddNew">
-                <p>Add new Post: </p>
-
-            </div>}
-
-            {postContext.posts.map(x => {
-                const comments = postContext.comments.filter(comment => comment.postId === x.id);
+            {posts.map(x => {
+                const postComments = comments.filter(comment => comment.postId === x.id);
                 return (
                     <Post
                         key={x.id}
                         post={x}
-                        comments={comments}
+                        comments={postComments}
                     ></Post>
                 )
-            })                // .sort(() => Math.random() - 0.5)
-            }
-        </div >
-
+            })}
+        </div>
     );
 }
 
-export default PostList;
+export default Posts;
