76d8d4777ed0f52a0c68530e966d1567014c2518
Krystian Petek <krystianpetek2@gmail.com>
2022-11-18 15:31:39 +0100

    Add reusable SearchInput

---
+++ fake-instagram-react-ts/src/components/Shared/SearchInput/SearchInput.tsx
@@ -0,0 +1,24 @@
+import { FunctionComponent } from "react";
+import "./SearchInput.scss";
+interface SearchInputProps {
+    id: string,
+    placeholder: string,
+    name: string,
+    handleSetId: (event: React.ChangeEvent<HTMLInputElement>) => void,
+    fetchData: (event: React.KeyboardEvent<HTMLInputElement>) => Promise<void>
+}
+
+const SearchInput: FunctionComponent<SearchInputProps> = ({ id, handleSetId, fetchData, name, placeholder }) => {
+    return (
+        <input className="SearchInput"
+            id={name}
+            name={name}
+            placeholder={placeholder}
+            min="1"
+            type="text"
+            value={id}
+            onChange={handleSetId}
+            onKeyDown={fetchData} />
+    );
+}
+export default SearchInput;
