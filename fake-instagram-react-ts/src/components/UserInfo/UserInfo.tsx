ceeda26511a1493dd4460b59f66be021f240a129
Krystian Petek <krystianpetek2@gmail.com>
2022-11-19 19:25:49 +0100

    Add handling to edit information about logged user

--- fake-instagram-react-ts/src/components/UserInfo/UserInfo.tsx
+++ fake-instagram-react-ts/src/components/UserInfo/UserInfo.tsx
@@ -1,60 +1,101 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useContext, useState } from "react";
 import "./UserInfo.scss";
 import { MdHomeWork, MdPermIdentity, MdHome, MdEmail, MdPhone, MdWeb } from "react-icons/md";
 import IUserResponse from "../../API/Response/IUserResponse";
+import InputUserInfo from "../InputUserInfo/InputUserInfo";
+import { ILoginContext, LoginContext } from "../../contexts/LoginContext/LoginContext";
+import Button from "../Shared/Button/Button";
 
 interface UserInfoProps {
     user: IUserResponse | null
 }
 const UserInfo: FunctionComponent<UserInfoProps> = ({ user }) => {
+    const { setEmail, setUsername } = useContext<ILoginContext>(LoginContext)
+
+    const [buttonLocked, setButtonLocked] = useState<boolean>(true);
+
+    const handleOnChange = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        if (name === "email")
+            setEmail(value);
+        else if (name === "username")
+            setUsername(value);
+
+        setUserInfo(prevValue => {
+            console.log(prevValue)
+
+            let returnValue = (
+                {
+                    ...prevValue, [name]: [value],
+
+                });
+
+            if (Object.keys(prevValue.address).includes(e.target.name)) {
+                returnValue.address = { ...returnValue.address, [name]: [value] }
+            }
+            if (Object.keys(prevValue.address).includes(e.target.name)) {
+                returnValue.company = { ...returnValue.company, [name]: [value] }
+            }
+            return returnValue;
+        })
+    };
+
+    const handleOnClick = () => {
+        setButtonLocked(prevValue => !prevValue);
+    }
+
+    const [userInfo, setUserInfo] = useState<IUserResponse>(user!);
+
     return (
         <div className="UserInfo">
-            <div>
+            <div className="UserInfo__Container">
                 <MdPermIdentity />
                 <div>
-                    <p>ID: {user?.id}</p>
-                    <p>Username: {user?.username}</p>
-                    <p>Name: {user?.name}</p>
+                    {buttonLocked ? <p>ID: {userInfo.id}</p> :
+                        <InputUserInfo handleOnChange={handleOnChange} name="id" placeholder="ID: " type="text" value={userInfo.id} />}
+
+                    {buttonLocked ? <p>Username: {userInfo.username}</p> :
+                        <InputUserInfo handleOnChange={handleOnChange} readOnly name="username" placeholder="Username: " type="text" value={userInfo.username} />}
+
+                    {buttonLocked ? <p>Name: {userInfo.name}</p> :
+                        <InputUserInfo handleOnChange={handleOnChange} name="name" placeholder="Name: " type="text" value={userInfo.name} />}
                 </div>
             </div>
 
-            <div>
+            <div className="UserInfo__Container">
                 <MdEmail />
                 <div>
-                    <p>Email: {user?.email}</p>
+                    {buttonLocked ? <p>Email: {userInfo.email}</p> :
+                        <InputUserInfo handleOnChange={handleOnChange} readOnly name="email" placeholder="Email: " type="text" value={userInfo.email} />}
                 </div>
             </div>
 
-            <div>
+            <div className="UserInfo__Container">
                 <MdPhone />
                 <div>
-                    <p>Phone: {user?.phone}</p>
+                    {buttonLocked ? <p>Phone: {userInfo.phone}</p> :
+                        <InputUserInfo handleOnChange={handleOnChange} name="phone" placeholder="Phone: " type="text" value={userInfo.phone} />}
                 </div>
             </div>
 
-            <div>
+            <div className="UserInfo__Container">
                 <MdHome />
                 <div>
-                    <p>Zipcode: {user?.address.zipcode}</p>
-                    <p>City: {user?.address.city}</p>
-                    <p>Street: {user?.address.street}</p>
-                    <p>Suite: {user?.address.suite}</p>
-                    <p>Geo: {user?.address.geo.lat} | {user?.address.geo.lng}</p>
+                    {buttonLocked ? <p>Zipcode: {userInfo.address.zipcode}</p> :
+                        <InputUserInfo handleOnChange={handleOnChange} name="zipcode" placeholder="Zipcode: " type="text" value={userInfo.address.zipcode} />}
+                    {buttonLocked ? <p>City: {userInfo.address.city}</p> :
+                        <InputUserInfo handleOnChange={handleOnChange} name="city" placeholder="City: " type="text" value={userInfo.address.city} />}
+                    {buttonLocked ? <p>Street: {userInfo.address.street}</p> :
+                        <InputUserInfo handleOnChange={handleOnChange} name="street" placeholder="Street: " type="text" value={userInfo.address.street} />}
+                    {buttonLocked ? <p>Suite: {userInfo.address.suite}</p> :
+                        <InputUserInfo handleOnChange={handleOnChange} name="suite" placeholder="Suite: " type="text" value={userInfo.address.suite} />}
+                    <p>Geo: {userInfo.address.geo.lat} | {userInfo.address.geo.lng}</p>
                 </div>
             </div>
 
-            <div>
-                <MdHomeWork />
-                <div>
-                    <p>Zipcode: {user?.address.zipcode}</p>
-                    <p>City: {user?.address.city}</p>
-                    <p>Street: {user?.address.street}</p>
-                    <p>Suite: {user?.address.suite}</p>
-                    <p>Geo: {user?.address.geo.lat} | {user?.address.geo.lng}</p>
-                </div>
-            </div>
-
-            <div>
+            <div className="UserInfo__Container">
                 <MdHomeWork />
                 <div>
                     <p>Company: {user?.company?.name}</p>
@@ -62,13 +103,15 @@ const UserInfo: FunctionComponent<UserInfoProps> = ({ user }) => {
                     <p>{user?.company?.bs}</p>
                 </div>
             </div>
-            <div>
+            <div className="UserInfo__Container">
                 <MdWeb />
                 <div>
-                    <p>Website: {user?.website}</p>
+                    {buttonLocked ? <p>Website: {userInfo.website}</p> :
+                        <InputUserInfo handleOnChange={handleOnChange} name="website" placeholder="Website: " type="text" value={userInfo.website} />}
                 </div>
             </div>
-        </div>
+            <Button onClick={handleOnClick}>EDIT</Button>
+        </div >
     );
 }
 
