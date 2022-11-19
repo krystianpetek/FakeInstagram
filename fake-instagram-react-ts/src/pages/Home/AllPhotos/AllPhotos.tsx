92e88cfc1802a6c2a874b69851d5265ab03acaad
Krystian Petek <krystianpetek2@gmail.com>
2022-11-19 13:19:34 +0100

    AllPhotos component to ensure view with all photos in Home page

---
+++ fake-instagram-react-ts/src/pages/Home/AllPhotos/AllPhotos.tsx
@@ -0,0 +1,30 @@
+import { FunctionComponent } from "react";
+import IPhotoResponse from "../../../API/Response/IPhotoResponse";
+import PhotoInfo from "../../../components/PhotoInfo/PhotoInfo";
+import Button from "../../../components/Shared/Button/Button";
+import "./AllPhotos.scss";
+
+interface AllPhotosProps {
+    photos?: Array<IPhotoResponse> | null,
+    lastPhoto?: number,
+    handleSetLastPhoto: () => void,
+}
+const AllPhotos: FunctionComponent<AllPhotosProps> = ({ photos, lastPhoto, handleSetLastPhoto }) => {
+    return (
+        <div className="AllPhotos">
+            < div className="button" >
+                <Button onClick={handleSetLastPhoto}>Load more...</Button>
+            </div >
+
+            <div className="photo">
+                {photos?.slice(0, lastPhoto)
+                    .map(photo => {
+                        return <PhotoInfo photo={photo} key={photo.id} />
+                    })
+                }
+            </div>
+        </div>
+    );
+}
+
+export default AllPhotos;
