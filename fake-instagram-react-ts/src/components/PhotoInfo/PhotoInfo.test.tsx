f96de28c4dcb5a8e74b2527b963f0ae310d78db0
Krystian Petek <krystianpetek2@gmail.com>
2023-03-20 07:26:22 +0100

    Added tests for PhotoInfo and LoginContext components

---
+++ fake-instagram-react-ts/src/components/PhotoInfo/PhotoInfo.test.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { screen, render } from '@testing-library/react';
+import PhotoInfo from "./PhotoInfo";
+import IPhotoResponse from "../../API/Response/IPhotoResponse";
+
+let mockPhoto: IPhotoResponse = {
+    albumId: 1,
+    id: 13,
+    title: "repudiandae iusto deleniti rerum",
+    url: "https://via.placeholder.com/600/197d29",
+    thumbnailUrl: "https://via.placeholder.com/150/197d29"
+};
+
+describe('should render PhotoInfo component', () => {
+    it('information about photo exists on screen', () => {
+        render(<PhotoInfo photo={mockPhoto}></PhotoInfo>);
+        const renderThumbnail: HTMLDivElement = screen.getByText(mockPhoto.thumbnailUrl);
+        const renderTitle: HTMLDivElement = screen.getByText(mockPhoto.title);
+        const renderId: HTMLDivElement = screen.getByText((content) => content.endsWith(`${mockPhoto.id}`));
+        const renderAlbumId = screen.getByText((content) => content.endsWith(`${mockPhoto.albumId}`));
+
+        expect(renderThumbnail.textContent).toEqual(mockPhoto.thumbnailUrl);
+        expect(renderTitle.textContent).toEqual(mockPhoto.title);
+        expect(renderId.textContent).toEqual(`ID: ${mockPhoto.id}`);
+        expect(renderAlbumId.textContent).toEqual(`AlbumId: ${mockPhoto.albumId}`);
+    });
+
+    it('photo exists on screen', () => {
+        render(<PhotoInfo photo={mockPhoto}></PhotoInfo>);
+        const renderImage: HTMLImageElement = screen.getByAltText(mockPhoto.title);
+        expect(renderImage.src).toEqual(mockPhoto.url);
+    })
+});
---
+++ fake-instagram-react-ts/src/contexts/LoginContext/LoginContext.test.tsx
@@ -0,0 +1,50 @@
+import { render, screen } from "@testing-library/react";
+import React, { FC, useEffect } from 'react';
+import { ILoginContext, LoginContext } from "./LoginContext";
+import { LoginContextProvider } from "./LoginContextProvider";
+import { useContext } from "react";
+
+
+describe('LoginContextProvider', () => {
+
+  const TestComponent: FC<{ name?: string, email?: string }> = (props) => {
+
+    const { email, setEmail, username, setUsername } = useContext<ILoginContext>(LoginContext);
+
+    useEffect(() => {
+      setUsername(props.name ?? '');
+      setEmail(props.email ?? '')
+    }, []);
+
+    return <>
+      <p data-testid='username'>{username}</p>
+      <p data-testid='email'>{email}</p>
+    </>
+  };
+
+  it('user isn\'t logged', () => {
+    render(<LoginContextProvider>
+      <TestComponent />
+      {/* <TestComponent name="krystian" email="petek@wsei.tempmail.pl" /> */}
+    </LoginContextProvider>);
+
+    const renderUser = screen.getByTestId('username');
+    const renderEmail = screen.getByTestId('email');
+
+    expect(renderUser.textContent).toEqual('');
+    expect(renderEmail.textContent).toEqual('');
+  });
+
+  it('user logged as "krystian"', () => {
+    render(<LoginContextProvider>
+      <TestComponent name="krystian" email="petek@wsei.tempmail.pl" />
+    </LoginContextProvider>);
+
+    const renderUser = screen.getByTestId('username');
+    const renderEmail = screen.getByTestId('email');
+
+    expect(renderUser.textContent).toEqual('krystian');
+    expect(renderEmail.textContent).toEqual('petek@wsei.tempmail.pl');
+  });
+
+});
